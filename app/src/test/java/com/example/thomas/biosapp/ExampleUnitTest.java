5b519d78b7335010fce564e3159ed9953c036f3b
Thomas De Lange <thomas.m.delange@gmail.com>
2018-03-20 18:05:27 +0100

    Testing circleci again

--- app/src/test/java/com/example/thomas/biosapp/ExampleUnitTest.java
+++ app/src/test/java/com/example/thomas/biosapp/ExampleUnitTest.java
@@ -14,7 +14,7 @@ public class ExampleUnitTest {
     @Test
     @Ignore
     public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
-        
+        assertEquals(5, 2 + 2);
+
     }
 }
