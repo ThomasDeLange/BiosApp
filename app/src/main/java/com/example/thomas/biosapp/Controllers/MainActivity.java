b834c22ecfcf590dd0488369f17cbf0504b69f37
Thomas De Lange <thomas.m.delange@gmail.com>
2018-03-21 14:06:39 +0100

    Versie 1.1
    Added
    GridView on main activity

---
+++ app/src/main/java/com/example/thomas/biosapp/Controllers/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.thomas.biosapp.Controllers;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
+
+import com.example.thomas.biosapp.R;
+import com.example.thomas.biosapp.Util.FilmGridAdapter;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        GridView gridview = (GridView) findViewById(R.id.FilmGridView);
+        gridview.setAdapter(new FilmGridAdapter(this));
+
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+                Toast.makeText(MainActivity.this, "" + position, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
