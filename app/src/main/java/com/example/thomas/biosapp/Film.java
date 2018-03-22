a09a1e95972f5d2cefbb35c9209f49d684d4a29d
Stephan de Wit <swit2@avans.nl>
2018-03-22 13:17:45 +0100

    updated Film-domain

--- app/src/main/java/com/example/thomas/biosapp/Film.java
+++ app/src/main/java/com/example/thomas/biosapp/Film.java
@@ -6,9 +6,16 @@ package com.example.thomas.biosapp;
 
 public class Film {
     private String name;
+    private String posterUrl;
+    private String description;
+    private String id;
 
-    public Film(String name) {
+    public Film(String name, String posterUrl, String description,
+                String id) {
         this.name = name;
+        this.posterUrl = posterUrl;
+        this.description = description;
+        this.id = id;
     }
 
     public String getName() {
@@ -17,5 +24,29 @@ public class Film {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getPosterUrl() {
+        return posterUrl;
+    }
+
+    public void setPosterUrl(String posterUrl) {
+        this.posterUrl = posterUrl;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
     }
 }
