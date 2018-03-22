73c1fd4ea14bbd3b43e202ccaf3054b9df5e53bd
Thomas De Lange <thomas.m.delange@gmail.com>
2018-03-22 15:09:37 +0100

    ContactScreen versie 1.0
    Added activity + Maps activity

---
+++ app/src/main/java/com/example/thomas/biosapp/Controllers/ContactActivity.java
@@ -0,0 +1,15 @@
+package com.example.thomas.biosapp.Controllers;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.thomas.biosapp.R;
+
+public class ContactActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact);
+    }
+}
