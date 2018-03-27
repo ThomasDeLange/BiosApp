aafe2707ac0db980b6ac03563a67150e4d4edc34
Julian van Noordennen <jnoorden@avans.nl>
2018-03-27 11:58:23 +0200

    Ticket Betaling v1.0
    Selectie paymentmethode toegevoegd

---
+++ app/src/main/java/com/example/thomas/biosapp/Api/PaymentMethodActivity.java
@@ -0,0 +1,15 @@
+package com.example.thomas.biosapp.Api;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.thomas.biosapp.R;
+
+public class PaymentMethodActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payment_method);
+    }
+}
