81389e85477c9052a365816b8b8d51476729dd38
Julian van Noordennen <jnoorden@avans.nl>
2018-03-27 12:07:41 +0200

    Ticket Betaling V1.1
    Doorstuur naar Activiteit gemaakt

--- app/src/main/java/com/example/thomas/biosapp/Controllers/MainActivity.java
+++ app/src/main/java/com/example/thomas/biosapp/Controllers/MainActivity.java
@@ -10,6 +10,7 @@ import android.widget.GridView;
 
 import com.example.thomas.biosapp.Api.FilmTask;
 import com.example.thomas.biosapp.Api.OnFilmAvailable;
+import com.example.thomas.biosapp.Api.PaymentMethodActivity;
 import com.example.thomas.biosapp.Domain.Film;
 import com.example.thomas.biosapp.R;
 import com.example.thomas.biosapp.Util.FilmGridAdapter;
@@ -25,6 +26,11 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        //Hack
+        Intent intent = new Intent(getApplicationContext(), PaymentMethodActivity.class);
+        startActivity(intent);
 
         films = new ArrayList<>();
 
