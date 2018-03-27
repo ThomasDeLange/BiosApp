8db4e5fa7d1edf9c47751c9d3d2246fbab5f3156
Thomas De Lange <thomas.m.delange@gmail.com>
2018-03-27 13:22:58 +0200

    Ticketbestelling versie 1.7
    Updated
    Detailed view to new layout

--- app/src/main/java/com/example/thomas/biosapp/Database/TicketDatabase/TicketDatabase.java
+++ app/src/main/java/com/example/thomas/biosapp/Database/TicketDatabase/TicketDatabase.java
@@ -53,7 +53,7 @@ public class TicketDatabase extends SQLiteOpenHelper implements Serializable {
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-        sqLiteDatabase.execSQL("CREATE TABLE `" + TICKET_TABLE_NAME + "` ( `" + TICKET_TICKET_ID + "` INTEGER NOT NULL, `" + TICKET_BUYER_ID + "` INTEGER NOT NULL, `" + TICKET_QR_CODE + "` TEXT NOT NULL, `" + TICKET_FILM_TITLE + "` TEXT NOT NULL, `" + TICKET_RUN_TIME + "` TEXT NOT NULL, `" + TICKET_SEAT_ID + "` INTEGER NOT NULL, FOREIGN KEY(`" + TICKET_BUYER_ID + "`) REFERENCES `" + BUYER_TABLE_NAME + "`(`" + BUYER_BUYER_ID + "`) ON UPDATE CASCADE ON DELETE CASCADE, FOREIGN KEY(`" + TICKET_SEAT_ID + "`) REFERENCES `" + SEAT_TABLE_NAME + "`(`" + SEAT_SEAT_ID + "`) ON UPDATE CASCADE ON DELETE CASCADE, PRIMARY KEY(`" + TICKET_TICKET_ID + "`) )");
+        sqLiteDatabase.execSQL("CREATE TABLE `" + TICKET_TABLE_NAME + "` ( `" + TICKET_TICKET_ID + "` INTEGER NOT NULL, `" + TICKET_BUYER_ID + "` INTEGER NOT NULL, `" + TICKET_QR_CODE + "` TEXT NOT NULL, `" + TICKET_FILM_TITLE + "` TEXT NOT NULL, `" + TICKET_RUN_TIME + "` TEXT NOT NULL, `" + TICKET_SEAT_ID + "` INTEGER NOT NULL, FOREIGN KEY(`" + TICKET_BUYER_ID + "`) REFERENCES `" + BUYER_TABLE_NAME + "` (`" + BUYER_BUYER_ID + "`) ON UPDATE CASCADE ON DELETE CASCADE, FOREIGN KEY (`" + TICKET_SEAT_ID + "`) REFERENCES `" + SEAT_TABLE_NAME + "`(`" + SEAT_SEAT_ID + "`) ON UPDATE CASCADE ON DELETE CASCADE, PRIMARY KEY(`" + TICKET_TICKET_ID + "`) )");
         sqLiteDatabase.execSQL("CREATE TABLE `" + BUYER_TABLE_NAME + "` ( `" + BUYER_BUYER_ID + "` INTEGER NOT NULL, `" + BUYER_FIRST_NAME + "` TEXT NOT NULL, FOREIGN KEY(`" + BUYER_BUYER_ID + "`) REFERENCES `" + TICKET_TABLE_NAME + "`(`" + BUYER_BUYER_ID + "`) )");
         sqLiteDatabase.execSQL("CREATE TABLE `" + SEAT_TABLE_NAME + "` ( `" + SEAT_SEAT_ID + "` INTEGER NOT NULL, `" + SEAT_ROW_NUMBER + "` INTEGER NOT NULL, `" + SEAT_SEAT_NUMBER + "` INTEGER NOT NULL, PRIMARY KEY(`" + SEAT_SEAT_ID + "`) )");
     }
@@ -118,7 +118,7 @@ public class TicketDatabase extends SQLiteOpenHelper implements Serializable {
             String qrCode = cursor.getString(cursor.getColumnIndex(TICKET_QR_CODE));
             String filmTitle = cursor.getString(cursor.getColumnIndex(TICKET_FILM_TITLE));
             String runTime = cursor.getString(cursor.getColumnIndex(TICKET_RUN_TIME));
-            String seatId = cursor.getString(cursor.getColumnIndex(TICKET_QR_CODE));
+            String seatId = cursor.getString(cursor.getColumnIndex(TICKET_SEAT_ID));
 
             ticketArrayList.add(new Ticket(ticketID, buyerID, qrCode, filmTitle, runTime, seatId));
             cursor.moveToNext();
