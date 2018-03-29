312f7e7edd6814b97b981ce986a30b6dfa2fccd5
Thomas De Lange <thomas.m.delange@gmail.com>
2018-03-29 12:57:36 +0200

    Revert "Merge branch 'TabLayout' into ContactScreen"
    This reverts commit 786316d47d9278830463f165e5f515baa3ad6bbb, reversing
    changes made to f23b93d83b24ca5509b2feb424d7781bfe893c56.

--- app/src/main/java/com/example/thomas/biosapp/Util/FilmGridAdapter.java
+++ app/src/main/java/com/example/thomas/biosapp/Util/FilmGridAdapter.java
@@ -1,72 +1,59 @@
 package com.example.thomas.biosapp.Util;
 
 import android.content.Context;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
-import android.widget.TextView;
 
-import com.example.thomas.biosapp.Domain.Film;
 import com.example.thomas.biosapp.R;
-import com.squareup.picasso.Picasso;
 
-import java.util.ArrayList;
+//import com.example.thomas.biosapp.R;
+
 public class FilmGridAdapter extends BaseAdapter {
     private Context context;
-    private LayoutInflater mInflator;
-    private ArrayList mFilmArrayList;
+    // references to our images
+    private Integer[] mThumbIds = {
+            R.drawable.sample_2, R.drawable.sample_3,
+            R.drawable.sample_4, R.drawable.sample_5,
+            R.drawable.sample_6, R.drawable.sample_7,
+            R.drawable.sample_0, R.drawable.sample_1,
+            R.drawable.sample_2, R.drawable.sample_3,
+            R.drawable.sample_4, R.drawable.sample_5,
+            R.drawable.sample_6, R.drawable.sample_7,
+            R.drawable.sample_0, R.drawable.sample_1,
+            R.drawable.sample_2, R.drawable.sample_3,
+            R.drawable.sample_4, R.drawable.sample_5,
+            R.drawable.sample_6, R.drawable.sample_7
+    };
 
-    public FilmGridAdapter(Context context, LayoutInflater layoutInflater, ArrayList<Film> filmArrayList) {
+    public FilmGridAdapter(Context context) {
         this.context = context;
-        this.mInflator = layoutInflater;
-        this.mFilmArrayList = filmArrayList;
     }
 
     public int getCount() {
-        int size = mFilmArrayList.size();
-        return size;
+        return mThumbIds.length;
     }
 
     public Object getItem(int position) {
-        return mFilmArrayList.get(position);
+        return null;
     }
 
     public long getItemId(int position) {
-        return position;
+        return 0;
     }
 
     // create a new ImageView for each item referenced by the Adapter
     public View getView(int position, View convertView, ViewGroup parent) {
-//        ImageView imageView;
-//        if (convertView == null) {
-//            // if it's not recycled, initialize some attributes
-//            imageView = new ImageView(context);
-//        } else {
-//            imageView = (ImageView) convertView;
-//        }
-//
-//        imageView.setImageResource(mThumbIds[position]);
-//        return imageView;
-
-        ViewHolder viewHolder;
+        ImageView imageView;
         if (convertView == null) {
-            convertView = mInflator.inflate(R.layout.film_gridview_item, null);
-            viewHolder = new ViewHolder();
-            viewHolder.imageView = (ImageView) convertView.findViewById(R.id.filmGridImageView);
-            convertView.setTag(viewHolder);
+            // if it's not recycled, initialize some attributes
+            imageView = new ImageView(context);
         } else {
-            viewHolder = (ViewHolder) convertView.getTag();
+            imageView = (ImageView) convertView;
         }
 
-        Film film = (Film) mFilmArrayList.get(position);
-        Picasso.get().load(film.getPosterUrl()).into(viewHolder.imageView);
-        return convertView;
-    }
-
-    private static class ViewHolder {
-        public ImageView imageView;
-        public TextView movieTitle;
+        imageView.setImageResource(mThumbIds[position]);
+        return imageView;
     }
 }
