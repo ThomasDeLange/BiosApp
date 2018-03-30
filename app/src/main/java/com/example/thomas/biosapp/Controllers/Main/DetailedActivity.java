e22b766e6c28c470064db407d60922be255ba359
Thomas De Lange <thomas.m.delange@gmail.com>
2018-03-30 16:34:16 +0200

    Revert "Update merge"
    This reverts commit 7b24af1e5cd99810505b31cce9a70000942fa3e9.

--- app/src/main/java/com/example/thomas/biosapp/Controllers/Main/DetailedActivity.java
+++ app/src/main/java/com/example/thomas/biosapp/Controllers/Main/DetailedActivity.java
@@ -1,7 +1,10 @@
 package com.example.thomas.biosapp.Controllers.Main;
 
+<<<<<<< HEAD:app/src/main/java/com/example/thomas/biosapp/Controllers/DetailedActivity.java
+=======
 import android.content.Intent;
 import android.os.Bundle;
+>>>>>>> TicketBesteling:app/src/main/java/com/example/thomas/biosapp/Controllers/Main/DetailedActivity.java
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,8 +12,11 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+<<<<<<< HEAD:app/src/main/java/com/example/thomas/biosapp/Controllers/DetailedActivity.java
+=======
 import com.example.thomas.biosapp.Controllers.Seats.SeatsActivity;
 import com.example.thomas.biosapp.Domain.Film;
+>>>>>>> TicketBesteling:app/src/main/java/com/example/thomas/biosapp/Controllers/Main/DetailedActivity.java
 import com.example.thomas.biosapp.R;
 
 public class DetailedActivity extends AppCompatActivity implements View.OnClickListener {
@@ -30,10 +36,13 @@ public class DetailedActivity extends AppCompatActivity implements View.OnClickL
         TextView filmDetailedDescription = findViewById(R.id.filmDetailedDescription);
 
         //Meegestuurde gegevens verkrijgen
+<<<<<<< HEAD:app/src/main/java/com/example/thomas/biosapp/Controllers/DetailedActivity.java
         //Film film = (Film)getIntent().getSerializableExtra("FILM_OBJECT");
         //RequestCreator requestCreator = Picasso.with(getApplicationContext()).load(photo.getImageSource());
+=======
         film = (Film) getIntent().getSerializableExtra("FILM_OBJECT");
-        //RequestCreator requestCreator = Picasso.get().load(film.getPosterUrl());
+        RequestCreator requestCreator = Picasso.get().load(film.getPosterUrl());
+>>>>>>> TicketBesteling:app/src/main/java/com/example/thomas/biosapp/Controllers/Main/DetailedActivity.java
 
         //Data aanpassen
         //filmDetailedTitle.setText(film.set)
